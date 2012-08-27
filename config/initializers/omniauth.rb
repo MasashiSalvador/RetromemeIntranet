75f05d7958723926ec37fe0b5f90cbb0081e0261
MasashiSalvador <masashisalvador57d@gmail.com>
2012-08-27 19:25:25 +0900

    first release

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '397893486935947','c52367963642e06c8f99cc585b507dfd'
+  #to release
+  #provider :facebook, '397893486935947','c52367963642e06c8f99cc585b507dfd'
+  #to test
+  provider :facebook, '250814918369249', '71f405e2f1340cd473c43a59aca622ad'
 end
